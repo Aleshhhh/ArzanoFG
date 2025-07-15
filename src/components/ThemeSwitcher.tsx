69e9283ad03b9ae931bbe319a017a0263a9c4205
I Sayan non hanno paura del virus <aleshsh30@gmail.com>
2025-07-15 20:22:26 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <ThemeSwitcher>
    
    w

--- src/components/ThemeSwitcher.tsx
+++ src/components/ThemeSwitcher.tsx
@@ -2,18 +2,16 @@
 
 import { useAppContext } from '@/context/AppContext';
 import { Moon, Sun } from 'lucide-react';
-import { Button } from './ui/button';
 
 export function ThemeSwitcher() {
-  const { users, currentUser, setTheme } = useAppContext();
+  const { users, currentUser } = useAppContext();
   const currentTheme = currentUser ? users[currentUser].theme : 'light';
 
-  const toggleTheme = () => {
-    setTheme(currentTheme === 'light' ? 'dark' : 'light');
-  };
-
   return (
-    <div aria-label="Toggle theme">
+    <div
+      aria-label="Toggle theme"
+      className="relative flex h-5 w-5 items-center justify-center"
+    >
       <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
       <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
       <span className="sr-only">Toggle theme</span>
