99ca555323d5d1219d2fc5dd9e5f266992dc959c
I Sayan non hanno paura del virus <aleshsh30@gmail.com>
2025-07-30 11:14:08 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -2,7 +2,12 @@
 
 import { AppProvider } from '@/context/AppContext';
 import { ReactNode } from 'react';
+import { PrimeReactProvider } from 'primereact/api';
 
 export function Providers({ children }: { children: ReactNode }) {
-  return <AppProvider>{children}</AppProvider>;
+  return (
+    <PrimeReactProvider>
+      <AppProvider>{children}</AppProvider>
+    </PrimeReactProvider>
+  );
 }
